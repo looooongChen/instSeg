07f2f632d3b1b3d9c88cf027d6d1ae0d11c4da90
chen <chen@lfb.rwth-aachen.de>
2020-05-20 18:18:07 +0200

    update_loss

--- train_cvppp.py
+++ train_cvppp.py
@@ -26,5 +26,5 @@ train_data = {'image': list(map(cv2.imread,X)),
 # plt.show()
 
 model = instSegV2.InstSeg(config=config, base_dir='./', run_name='cvppp_aug')
-model.train(train_data, batch_size=4, epochs=300, augmentation=True)
+model.train(train_data, batch_size=4, epochs=300, augmentation=False)
 
